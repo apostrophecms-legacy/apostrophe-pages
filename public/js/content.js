5c9138e2bfac3d04f1af1ecd6405d328a5f2f8b4
Tom Boutell <tom@punkave.com>
2013-08-14 13:51:10 -0400

    Improved search filters work if you click the back button in any browser that preserves forms

--- public/js/content.js
+++ public/js/content.js
@@ -9,29 +9,29 @@ $(function() {
 
   // Show and hide search results by type
   $('body').on('click', '.apos-search-filter', function() {
-    var checked = $(this).prop('checked');
-    var name = $(this).attr('name');
-    var names = [];
-    $('.apos-result').each(function() {
-      var $result = $(this);
-      if (name === 'page') {
-        if (!_.contains(typeNames, $result.data('type'))) {
-          if (checked) {
-            $result.show();
-          } else {
-            $result.hide();
-          }
-        }
-      } else {
-        if ($result.data('type') === name) {
-          if (checked) {
-            $result.show();
-          } else {
-            $result.hide();
-          }
-        }
-      }
-    });
+    update();
   });
 
+  // So it works if we click the back button and had filters on
+  // (this is quite common)
+  update();
+
+  function update() {
+    var checked = {};
+    $('.apos-search-filter').each(function() {
+      checked[$(this).attr('name')] = $(this).prop('checked');
+    });
+    $('.apos-result').each(function() {
+      var $result = $(this);
+      var type = $result.data('type');
+      if (!_.contains(typeNames, $result.data('type'))) {
+        type = 'page';
+      }
+      if (checked[type]) {
+        $result.show();
+      } else {
+        $result.hide();
+      }
+    });
+  }
 });
