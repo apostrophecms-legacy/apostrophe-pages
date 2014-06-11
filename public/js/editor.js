0593eb2fbdbc80ce4918d59c590ce89315576d01
Stuart Romanek <stuart@punkave.com>
2014-06-11 13:36:25 -0400

    reorg Visit link opens in new tab

--- public/js/editor.js
+++ public/js/editor.js
@@ -421,7 +421,7 @@ function AposPages() {
               $li.find('.jqtree-element').append($('<span class="apos-reorganize-controls"></span>'))
               // Append a link to the jqtree-element div.
               // The link has a url '#node-[id]' and a data property 'node-id'.
-              var link = $('<a class="apos-visit"></a>');
+              var link = $('<a class="apos-visit" target="_blank"></a>');
               link.attr('data-node-id', node.id);
               link.attr('data-visit', '1');
               link.attr('href', '#');
@@ -443,7 +443,9 @@ function AposPages() {
             var node = $tree.tree('getNodeById', nodeId);
             // TODO: this is an assumption about where the root of the page tree
             // is being served
-            window.location.href = node.slug;
+            var tab = window.open(node.slug, '_blank');
+            tab.focus();
+            // window.location.href = ;
             return false;
           });
 
